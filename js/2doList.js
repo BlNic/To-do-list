609edc300ebc44a17ed6f1cca8f0b0999bd7a63f
Blanka Nicman <bjnicman@o2.pl>
2023-01-17 19:36:31 +0100

    addind new tasks, done

--- js/2doList.js
+++ js/2doList.js
@@ -1,3 +1,4 @@
+
 {
     const tasks = [
         {
@@ -19,10 +20,74 @@
 
         for (const task of tasks) {
             htmlString += `
-    <li style="text-decoration: line-through">
+    <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
     ${task.content}
     </li>
-    `;
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
+
+
+/* before remodeling
+
+{
+    const tasks = [
+        {
+            content: "obejrzeć lekcję",
+            done: false,
+        },
+        {
+            content: "zrobić nototaki",
+            done: true,
+        },
+        {
+            content: "odrobić pracę domową",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+    <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+    ${task.content}
+    </li>
+        `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
@@ -30,9 +95,27 @@
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            if (newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+        });
     };
 
     init();
 }
+*/
 
 /* 05:40 */
